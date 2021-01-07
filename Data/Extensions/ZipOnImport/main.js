65884b1de1489352c0589450577276ea1117297c
Sgeo <sgeoster@gmail.com>
2021-01-06 23:26:35 -0500

    Only block moves from content directory.

--- Data/Extensions/ZipOnImport/main.js
+++ Data/Extensions/ZipOnImport/main.js
@@ -1,10 +1,14 @@
 module.exports.activate = () => {
+
     let flashpoint = require("flashpoint-launcher");
     let child_process = require("child_process");
+    let path = require("path");
 
     flashpoint.games.onWillImportGame(curationImportState => {
         let curationPath = curationImportState.curationPath;
-        curationImportState.contentToMove = [];
+        curationImportState.contentToMove = curationImportState.contentToMove.filter(([source, dest]) => {
+            return path.join(curationPath, "content") !== source; // TODO: Overly reliant on matching Launcher implementation?
+        });
         let bluezip = child_process.spawn("..\\Utilities\\bluezip\\bluezip", [curationPath, "-no", "..\\Games"]);
         return new Promise((resolve, reject) => {
             bluezip.stdout.on("data", (data) => {
